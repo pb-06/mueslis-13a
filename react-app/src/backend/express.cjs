7585083ca72ca24691c5f9c4d209a688e5e0ef84
pb-06 <kkandomail@gmail.com>
2025-09-22 08:45:13 +0200

    Get mysql data

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,9 +1,37 @@
 const express = require('express');
+const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', // TODO - credential!
+    database: 'test'
+});
+
+
+app.get('/mueslis', (req, res)=>{
+    //conn.connect(err=>console.warn('connect err', err));
+    //console.log('conn', conn);
+
+    conn.query("SELECT id, name, price FROM mueslis", (err, result, fields)=>{
+        if (err) {
+            console.warn(err);
+        }
+        if (result) {
+            console.log(result);
+            console.log(fields);
+
+            res.status(200).json({err, result, fields});
+        } else {
+            res.status(403).json({err});
+        }
+    });
+});
 
 app.get((err, req, res) => {
     if (err) {
